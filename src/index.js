2bfe493a5be0b0b1599bc413ae913915e35dd5c3
kevin <kevin088.rejoice@gmail.com>
2023-04-10 17:10:11 +0530

    Add meta title & description

--- src/index.js
+++ src/index.js
@@ -7,9 +7,16 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Helmet } from 'react-helmet';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <div>
+      <Helmet>
+        <meta name="description" content="Deine Energiezukunft." />
+      </Helmet>
+    </div>
     <ToastContainer />
     <App />
   </React.StrictMode>,
